4b5fdf684a86e7d8291269feb74c47170d99d045
sabbir hossain <mdsabbir477470@gmail.com>
2023-06-03 23:51:36 +0600

    recurtion done

--- ricertion.php
+++ ricertion.php
@@ -8,4 +8,14 @@
     printN($i);
  }
 
-printN(1);
+// printN(1);
+function printNumber($counter, $end, $stepping=1){
+    if($counter>$end){
+        return;
+    }
+    echo $counter . "\n";
+    $counter += $stepping;
+    printNumber($counter, $end, $stepping);
+}
+
+printNumber(21, 37, 2);
